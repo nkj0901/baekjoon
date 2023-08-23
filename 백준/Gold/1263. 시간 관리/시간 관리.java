b2a601aa9b330992fd2a362132e5596d7b6cb622
nkj0901 <nkj0901@naver.com>
2023-08-23 23:07:51 +0900

    [Gold V] Title: 시간 관리, Time: 100 ms, Memory: 12056 KB -BaekjoonHub

---
+++ 백준/Gold/1263. 시간 관리/시간 관리.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o2[1]-o1[1];
+            }
+        });
+
+        int cur = arr[0][1];
+        for(int i = 0; i < N; i++){
+            if(cur < arr[i][1]) cur-=arr[i][0];
+            else cur = arr[i][1]-arr[i][0];
+        }
+
+        if(cur < 0) cur = -1;
+        System.out.println(cur);
+    }
+}
