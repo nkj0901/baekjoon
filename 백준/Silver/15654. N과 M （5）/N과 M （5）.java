cebf941897367ae87155d55fbedd4f4ae2cf9c85
nkj0901 <nkj0901@naver.com>
2023-10-19 18:14:08 +0900

    [Silver III] Title: N과 M (5), Time: 328 ms, Memory: 69764 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,10 +9,11 @@ public class Main {
     static int[] sel;
     static boolean[] visited;
     static int[] number;
-    static StringBuilder sb = new StringBuilder();
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        sb = new StringBuilder();
 
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
