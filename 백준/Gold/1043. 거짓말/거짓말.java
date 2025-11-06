b63bcbe677122cde5f27e373e32369fc1e021556
nkj0901 <nkj0901@naver.com>
2025-11-06 16:06:31 +0900

    [Gold IV] Title: 거짓말, Time: 68 ms, Memory: 11712 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,91 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] parent;
+
+    static int[] p;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int answer = 0;
+        p = new int[N+1];
 
-        List<Integer>[] party = new List[M + 1];
-        boolean[] check = new boolean[N + 1];
-
-        //파티정보
-        for (int i = 1; i <= M; i++) {
-            party[i] = new ArrayList<>();
-        }
-
-
-        //누가 알고있는지 체크
+        //진실을 아는 사람 array
         st = new StringTokenizer(br.readLine());
-        int t = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < t; i++) {
-            check[Integer.parseInt(st.nextToken())] = true;
+        int num = Integer.parseInt(st.nextToken());
+        int[] know = new int[num];
+        for(int i = 0; i < num; i++) {
+            know[i] = Integer.parseInt(st.nextToken());
         }
 
-        parent = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
+        //파티 참여 list 만들기
+        List<Integer>[] list = new ArrayList[M];
+        for(int i = 0; i < M; i++) {
+            list[i] = new ArrayList<>();
         }
 
-        //파티 정보 입력
-        for (int i = 1; i <= M; i++) {
+        for(int i = 1; i <= N; i++) {
+            make_set(i);
+        }
+
+        for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < num; j++) {
-                int cur = Integer.parseInt(st.nextToken());
-                party[i].add(cur);
-                //사람들 연결
-                if (j != 0) {
-                    union(cur, party[i].get(j - 1));
-                }
-            }
-        }
-        
-        //T인것을 찾아서 부모 노드가 같은 아이 다 T로 바꿔주기
-        for (int i = 1; i <= N; i++) {
-            if (check[i]) {
-                int p = findSet(i);
-                for (int j = 1; j <= N; j++) {
-                    if (p == findSet(j)) {
-                        check[j] = true;
-                    }
-                }
+            int part = Integer.parseInt(st.nextToken());
+            int first = 0;
+            for(int j = 0; j < part; j++) {
+                list[i].add(Integer.parseInt(st.nextToken()));
+
+                if(j == 0) first = list[i].get(j);
+                else union(first, list[i].get(j));
+
             }
         }
 
-        //파티 정보를 보면서 F만 있으면 ans++;
-        int ans = 0;
-        loop:
-        for (int i = 1; i <= M; i++) {
-            for (int j = 0; j < party[i].size(); j++) {
-                if (check[party[i].get(j)]) continue loop;
+        boolean[] isKnow = new boolean[N+1];
+        for(int i = 0; i < know.length; i++) {
+            int root = find_set(p[know[i]]);
+
+            for(int j = 1; j < N+1; j++) {
+                if(find_set(p[j]) == root) isKnow[j] = true;
             }
-            ans++;
         }
-        System.out.println(ans);
+
+        //몇번 과장할 수 있는지 찾아보기
+        for(int i = 0; i < M; i++) {
+            if(!isKnow[list[i].get(0)]) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
     }
 
-    public static int findSet(int x) {
-        if (parent[x] != x) {
-            parent[x] = findSet(parent[x]);
-            return parent[x];
-        } else return x;
+    private static void make_set(int x) {
+        p[x] = x;
     }
 
-    public static void union(int x, int y) {
-        parent[findSet(y)] = parent[findSet(x)];
+    private static int find_set(int x) {
+        if(p[x] == x) return p[x];
+        return find_set(p[x]);
+    }
+
+    private static void union(int a, int b) {
+        p[find_set(b)] = find_set(a);
     }
 }
