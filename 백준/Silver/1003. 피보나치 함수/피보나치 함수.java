8e9a8006d02589fd24d41d5175af5307e63cd92f
nkj0901 <nkj0901@naver.com>
2025-11-12 13:30:00 +0900

    [Silver III] Title: 피보나치 함수, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,39 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-		for (int t = 1; t <= T; t++) {
+        for(int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] memo = new int[2][N+3];
 
-			int N = Integer.parseInt(br.readLine());
-			int[][] dp = new int[N + 1][2];
+            memo[0][0] = 1;
+            memo[1][1] = 1;
 
-			for (int[] tmp : dp) {
-				Arrays.fill(tmp, -1);
-			}
+            for(int i = 2; i <= N; i++) {
+                memo[0][i] = memo[0][i-1] + memo[0][i-2];
+                memo[1][i] = memo[1][i-1] + memo[1][i-2];
+            }
 
-			dp[0][0] = 1; // 0호출 수
-			dp[0][1] = 0; // 0호출 수
-
-			if (N >= 1) {
-				dp[1][0] = 0; // 1호출 수
-				dp[1][1] = 1; // 1호출 수
-			}
-
-			if (N >= 2) {
-				for (int i = 2; i <= N; i++) {
-					dp[i][0] = dp[i-1][0]+dp[i-2][0];
-					dp[i][1] = dp[i-1][1]+dp[i-2][1];
-				}
-			}
-
-			System.out.println(dp[N][0] + " " + dp[N][1]);
-		}
-	}
+            System.out.println(memo[0][N] + " " + memo[1][N]);
+        }
+    }
 }
