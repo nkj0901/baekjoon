b9ecd741a5a51088e96801353124b7f90a366b43
nkj0901 <nkj0901@naver.com>
2025-11-13 14:01:33 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 12056 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -4,28 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N][3];
-		st = new StringTokenizer(br.readLine());
-		arr[0][0] = Integer.parseInt(st.nextToken());
-		arr[0][1] = Integer.parseInt(st.nextToken());
-		arr[0][2] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-		int ans = 0;
+        int[][] arr = new int[N][3];
+        st = new StringTokenizer(br.readLine());
+        arr[0][0] = Integer.parseInt(st.nextToken());
+        arr[0][1] = Integer.parseInt(st.nextToken());
+        arr[0][2] = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Math.min(arr[i-1][1], arr[i-1][2]) + Integer.parseInt(st.nextToken());
-			arr[i][1] = Math.min(arr[i-1][0], arr[i-1][2]) + Integer.parseInt(st.nextToken());
-			arr[i][2] = Math.min(arr[i-1][0], arr[i-1][1]) + Integer.parseInt(st.nextToken());
-		}
+        for(int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
 
-		int min = Math.min(Math.min(arr[N - 1][0], arr[N - 1][1]), arr[N - 1][2]);
-		System.out.println(min);
+            arr[i][0] = Math.min(arr[i-1][1], arr[i-1][2]) + Integer.parseInt(st.nextToken());
+            arr[i][1] = Math.min(arr[i-1][0], arr[i-1][2]) + Integer.parseInt(st.nextToken());
+            arr[i][2] = Math.min(arr[i-1][0], arr[i-1][1]) + Integer.parseInt(st.nextToken());
+        }
 
-	}
+        System.out.println(Math.min(Math.min(arr[N-1][0], arr[N-1][1]),arr[N-1][2]));
+    }
 }
