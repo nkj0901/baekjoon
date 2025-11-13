4d15678fa9056136551f7b9cf1e5a89ce1d0048e
nkj0901 <nkj0901@naver.com>
2025-11-14 08:41:22 +0900

    [Gold IV] Title: 동전 1, Time: 76 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,24 +1,34 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int n  = Integer.parseInt(st.nextToken());
+        int k  = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
         int[] dp = new int[k+1];
-        dp[0] = 1;
+        dp[0] = 0;
+        int INF = 50001;
 
         for(int i = 0; i < n; i++) {
-            int num = sc.nextInt();
-            for(int j = 1; j <= k; j++) {
-                if(num <= j) {
-                    dp[j]+=dp[j-num];
-                }
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < k+1; j++) {
+                if(j < coins[i]) continue;
+                dp[j] = dp[j-coins[i]] + (dp[j] == INF ? 0 : dp[j]);
+                if(j == coins[i]) dp[j]++;
             }
         }
+
         System.out.println(dp[k]);
     }
 }
