f137eaef40dcfc095e936de8adea2e7eb4377141
nkj0901 <nkj0901@naver.com>
2025-11-25 10:24:22 +0900

    [level 3] Title: 아이템 줍기, Time: 35.37 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,82 +1,114 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     
-    static class Node{
-        int r;
-        int c;
-        int cnt;
-        Node(int r, int c, int cnt) {
+    int[][] arr;
+    
+    class Node implements Comparable<Node> {
+        int cnt, r, c;
+        
+        Node(int cnt, int r, int c) {
+            this.cnt = cnt;
             this.r = r;
             this.c = c;
-            this.cnt = cnt;
+        }
+        
+        @Override
+        public int compareTo(Node o1) {
+            return this.cnt - o1.cnt;
         }
     }
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
-        int L = rectangle.length;
-        int[][] arr = new int[102][102];
-        int arrL = arr.length;
+        arr = new int[102][102];
         
-        for(int i = 0; i < L; i++) {
-            int[] curArr = rectangle[i];
-            for(int l = curArr[0]*2; l <= curArr[2]*2; l++) {
-                for(int r = curArr[1]*2; r <= curArr[3]*2; r++) {
-                    arr[l][r]=1;
-                }
-            }
-        }
-        int[] dr1 = {-1, -1, -1, 0, 1, 1, 1, 0};
-        int[] dc1 = {-1, 0, 1, 1, 1, 0, -1, -1};
-        //길 체크
-        for(int i = 0; i < arrL; i++) {
-            for(int j = 0; j < arrL; j++) {
-                if(arr[i][j] == 1) {
-                    for(int d = 0; d < 8; d++) {
-                        int nr = i + dr1[d];
-                        int nc = j + dc1[d];
-                        if(nr < 0 || nc < 0 || nr >= arrL || nc >= arrL) continue;
-                        if(arr[nr][nc] == 0) {
-                            arr[i][j] = 2;
-                            break;
-                        }
-                    }
+        for(int i = 0; i < rectangle.length; i++) {
+            for(int x = rectangle[i][0] * 2; x <= rectangle[i][2] * 2; x++) {
+                for(int y = rectangle[i][1] * 2; y <= rectangle[i][3] * 2; y++) {
+                    arr[x][y] = 1;
                 }
             }
         }
         
-        // for(int[] a : arr) {
-        //     System.out.println(Arrays.toString(a));
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+        
+        //외부 2로 다 만들어주기 (안에 구멍이 있을 때를 구별하기 위해)
+        boolean[][] visited = new boolean[102][102];
+        Queue<Node> q = new LinkedList();
+        q.add(new Node(0, 0, 0));
+        
+        while(!q.isEmpty()) {
+            Node n = q.poll();
+                        
+            if(visited[n.r][n.c]) continue;
+            visited[n.r][n.c] = true;
+
+            for(int d = 0; d < 4; d++) {
+                int nr = n.r + dr[d];
+                int nc = n.c + dc[d];
+                
+                if(nr < 0 || nc < 0 || nr >= 102 || nc >= 102) continue;
+                if(arr[nr][nc] != 0) continue;
+                arr[nr][nc] = 2;
+                q.add(new Node(0, nr, nc));
+            }
+        }
+        // for(int i = 0; i < 30; i++) {
+        //     for(int j = 0; j < 30; j++) {
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println();
         // }
+
+        visited = new boolean[102][102]; 
+        PriorityQueue<Node> pq = new PriorityQueue();
+        pq.add(new Node(0, characterX * 2, characterY * 2));
         
         
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(characterX*2, characterY*2, 0));
-        
-        int[] dr2 = {-1, 0, 1, 0};
-        int[] dc2 = {0, 1, 0, -1};
-        while(!queue.isEmpty()) {
-            Node n = queue.poll();
-            //다시 방문하지 않도록 체크
-            arr[n.r][n.c] = 1;
+        while(!pq.isEmpty()) {
+            Node n = pq.poll();
             
-            if(n.r == itemX*2 && n.c == itemY*2) {
+            if(n.r == itemX * 2 && n.c == itemY * 2) {
                 answer = n.cnt/2;
                 break;
             }
             
+            if(visited[n.r][n.c]) continue;
+            visited[n.r][n.c] = true;
+            
+            // System.out.println(n.r + " " + n.c + " cnt : " + n.cnt);
+            
             for(int d = 0; d < 4; d++) {
-                //2인 애들 다 넣어주기
-                int nr = n.r + dr2[d];
-                int nc = n.c + dc2[d];
-                if(nr < 0 || nc < 0 || nr >= arrL || nc >= arrL) continue;
-                if(arr[nr][nc] == 2) {
-                    queue.add(new Node(nr, nc, n.cnt+1));
-                }
+                int nr = n.r + dr[d];
+                int nc = n.c + dc[d];
+                
+                //1 아니면 패스
+                if(arr[nr][nc] != 1)  continue;
+                //테두리 아니면 패스
+                if(!isOutline(nr, nc)) continue; 
+                pq.add(new Node(n.cnt+1, nr, nc));
             }
+      
         }
-    
+        
+        
         return answer;
+    }
+    
+    private boolean isOutline(int r, int c) {
+        int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+        int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+        
+        for(int d = 0; d < 8; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+            
+            if(arr[nr][nc] == 2) return true;
+        }
+        
+        return false;        
     }
 }
